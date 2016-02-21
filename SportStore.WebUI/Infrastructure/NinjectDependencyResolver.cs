e727782c7f6a6a88e12448cb45dd27ba0f8ba984
Matthijs Overboom <matthijsske@gmail.com>
2016-02-21 22:08:31 +0100

    init commit

---
+++ SportStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ninject;
+using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Entities;
+
+namespace SportStore.WebUI.Infrastructure {
+    public class NinjectDependencyResolver : IDependencyResolver {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam) {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType) {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType) {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings() {
+            //set bindings
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
